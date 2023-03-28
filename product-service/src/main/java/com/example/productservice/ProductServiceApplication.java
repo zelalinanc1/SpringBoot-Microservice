3513fa530435d20c94f168d972f48a4f9b77ac95
Zelal Inanc <zelalinanc21@gmail.com>
2023-03-29 01:06:20 +0300

    Discovery Server modulu eklendi.

--- product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
@@ -4,10 +4,12 @@ import com.example.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
 @RequiredArgsConstructor
+@EnableDiscoveryClient
 public class ProductServiceApplication {
 	private final ProductRepository productRepository;
 	public static void main(String[] args) {
