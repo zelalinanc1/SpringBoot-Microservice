2b94481c119cc423eb40bc22aef2d2fc5e301537
Zelal Inanc <zelalinanc21@gmail.com>
2023-03-30 18:36:07 +0300

    Api Gateway eklendi

--- discovery-server/src/main/java/com/example/apigateway/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/example/apigateway/discoveryserver/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package org.example.discoveryserver;
+package com.example.apigateway.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
