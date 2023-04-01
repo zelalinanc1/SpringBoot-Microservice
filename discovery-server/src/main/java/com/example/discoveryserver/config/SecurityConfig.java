74e9cbaa12f8f7c99d22296d6dcec96cddf9b001
Zelal Inanc <zelalinanc21@gmail.com>
2023-04-01 13:28:51 +0300

    Security Congig dosyasında bazı degisiklikler yapildi

--- discovery-server/src/main/java/com/example/discoveryserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/com/example/discoveryserver/config/SecurityConfig.java
@@ -7,15 +7,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
+
 @Configuration
-public class SecurityConfig{
+public class SecurityConfig {
 
     @Value("${eureka.username}")
     private String username;
@@ -24,25 +29,28 @@ public class SecurityConfig{
     private String password;
 
 
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .withUser(username).password(password)
-                .authorities("USER");
-
-
-
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username(username)
+                .password(password)
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 
 
-
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests().anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
 
+        return http.build();
+
 
     }
+
 }
