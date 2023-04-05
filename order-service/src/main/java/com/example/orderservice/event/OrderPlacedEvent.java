11a20c0a9a19ec1deb5e4c6b4646f2911f1c4ab2
Zelal Inanc <zelalinanc21@gmail.com>
2023-04-05 16:34:22 +0300

    Kafka eklendi

---
+++ order-service/src/main/java/com/example/orderservice/event/OrderPlacedEvent.java
@@ -0,0 +1,14 @@
+package com.example.orderservice.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderPlacedEvent {
+
+    private String orderNumber;
+
+}
--- order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -3,10 +3,12 @@ package com.example.orderservice.service;
 import com.example.orderservice.dto.InventoryResponse;
 import com.example.orderservice.dto.OrderRequest;
 import com.example.orderservice.dto.orderLineItemsDto;
+import com.example.orderservice.event.OrderPlacedEvent;
 import com.example.orderservice.model.Order;
 import com.example.orderservice.model.OrderLineItems;
 import com.example.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -22,6 +24,9 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+
+    private final KafkaTemplate<String,OrderPlacedEvent> kafkaTemplate;
+
     public String placeOrder(OrderRequest orderRequest){
         Order order = new Order();
 
@@ -49,6 +54,7 @@ public class OrderService {
 
         if(allProductsInStock){
             orderRepository.save(order);
+            kafkaTemplate.send("notificationTopic", new OrderPlacedEvent( order.getOrderNumber()));
             return "Order Placed Successfully";
         }else {
            throw  new IllegalArgumentException("Product is not in stock,please try again later");
